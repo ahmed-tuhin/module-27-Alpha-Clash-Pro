ed15944e774de20a1e4810bef00cbf4549efbd12
ahmed-tuhin <ahmedtuhin1218@gmail.com>
2024-02-16 00:27:37 +0600

    try function method

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -29,11 +29,53 @@ function handleKeyboardButtonPress(event){
     if( playerPressed === expectedAlphabet){
         console.log('you get a point');
         console.log('you have pressed correctly',expectedAlphabet)
+
+        const currentScore = getTextElementValueById('current-score');
+        console.log(currentScore);
+        const updatedScore = currentScore + 1 ; 
+        setTextElementValueById('current-score',updatedScore)
+
+
+
+
+
+    //------------------------------------------------------------------------------------
+    //      update score
+    //      1. get the current score
+    //     const currentScoreElement = document.getElementById('current-score');
+    //     const currentScoreText = currentScoreElement.innerText;
+    //     const currentScore = parseInt(currentScoreText);
+    //     console.log(currentScore);
+
+    //     // 2. increase the score by 1 
+    //     const newScore = currentScore + 1;
+    //     // 3. show the  update score 
+    //    currentScoreElement.innerText = newScore;
+    //------------------------------------------------------------------------------------
+        // start a new round
         continueGame();
         removeBackgroundColorById(expectedAlphabet)
     }
     else{
         console.log('you missed .you lost a life');
+
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life',updatedLife)
+
+
+
+        //-----------------------------------------------------------------------------------
+        // // step-1:  get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText)
+        // // step-1: reduce the life count 
+        // const newLife = currentLife - 1;
+        // // step-1: display the update life count
+        // currentLifeElement.innerText = newLife;
+        //----------------------------------------------------------------------------------
     }
 }
 
